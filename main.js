99bf9ff9f4b8cf5f811a92b67ac27a2c6d537a7b
Student <student@Mac-10ddb1e3f1f9.local>
2015-09-07 13:12:30 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+for(var i = 0; i < 5; i++){
+    console.log(i);
+}
+
+for(var i = 0; i < 600; i+=100){
+    console.log(i);
+}
+
+for(var j = 1; j < 65; j*=2){
+    console.log(j);
+}
+for(var j = 0; j < 11; j+=2){
+    console.log(j);
+}
+for(var j = 9; j >= 0; j--){
+    console.log(j);
+}
+for(var i = 0; i < 4; i+=1){
+  
+
+    for(var j = 0; j < 4; j+=1)
+  
+    
+    console.log(j);
+}
